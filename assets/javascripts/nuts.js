fa0104d799b40b20628cd926dced2b078ce9eb37
alookatommorow <hessjohnt@gmail.com>
2016-05-13 20:52:03 -0700

    add image rotator

--- assets/javascripts/nuts.js
+++ assets/javascripts/nuts.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+  var rotator = new ImageRotator();
+  rotator.init();
+  var rotationTimer = setInterval(rotator.cycleImages, 4000);
 
   if (window.location.pathname !== '/') {
     $(".blog-hide").hide();
@@ -31,7 +34,7 @@ $(document).ready(function() {
   //smooth scroll
   function smoothScroll(target) {
     $('html, body').animate({
-        scrollTop: $($(target).attr('href')).offset().top
+        scrollTop: $($(target).attr('href')).offset().top - 100
     }, 500);
   }
 
@@ -44,3 +47,54 @@ $(document).ready(function() {
   //   $(this).addClass('animated bounceOutLeft');
   // })
 });
+
+
+function ImageRotator() {
+  var $container = $('.image-rotator');
+  var currentUrl = 0;
+  var imageUrls = [];
+
+  this.init = function() {
+    setInitialImage(window.location.pathname);
+    setImageUrls(window.location.pathname);
+  }
+
+  this.cycleImages = function () {
+    currentUrl++;
+    if (currentUrl === imageUrls.length) {
+      currentUrl = 0;
+    }
+    animate(imageUrls[currentUrl]);
+  };
+
+  function googleBucketUrlFrom(url) {
+    return "url('https://storage.googleapis.com/john-hess/portfolio/"+url+".png')";
+  }
+
+  function animate(url) {
+    $container.fadeOut(1500, function() {
+      $container.css('background-image', googleBucketUrlFrom(url));
+      $container.fadeIn(1500);
+    });
+  }
+
+  function setImageUrls(location) {
+    if (location === '/portfolio/west-coast-skateparks/') {
+      imageUrls = ['wcs', 'wcs2', 'wcs3', 'wcs4'];
+    } else if (location === '/portfolio/music-tree/') {
+      imageUrls = ['mt', 'mt2', 'mt3', 'mt4'];
+    } else if (location === '/portfolio/repo-ranker/') {
+      imageUrls = ['rr', 'rr2', 'rr3'];
+    }
+  }
+
+  function setInitialImage(location) {
+    if (location === '/portfolio/west-coast-skateparks/') {
+      $container.css('background-image', googleBucketUrlFrom('wcs'));
+    } else if (location === '/portfolio/music-tree/') {
+      $container.css('background-image', googleBucketUrlFrom('mt'));
+    } else if (location === '/portfolio/repo-ranker/') {
+      $container.css('background-image', googleBucketUrlFrom('rr'));
+    }
+  }
+}
